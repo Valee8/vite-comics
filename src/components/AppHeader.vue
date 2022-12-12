08b5f4d7f50e40eb13d02a38bfa8fae8cc1eb04f
Vale <oirelav95@gmail.com>
2022-12-12 15:16:08 +0100

    creo l'header in template

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+};
+</script>
+
+<template>
+
+    <header>
+        
+    </header>
+    
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    background-color: #fff;
+}
+
+</style>
