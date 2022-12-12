2509d8b74079e3adc7a61fd43a822c1835fb0285
Vale <oirelav95@gmail.com>
2022-12-12 16:13:51 +0100

    modifico stili nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -60,14 +60,15 @@ export default {
 
     <header>
         <a href="#">
-            <img :src="getImagePath(`/img/${imgLogo}`)" alt="Logo DC">
-        </a>
+                <img :src="getImagePath(`/img/${imgLogo}`)" alt="Logo DC">
+            </a>
 
         <nav>
             <ul>
                 <li v-for="links in headerLinks">
                     <a :href="links.url">
                         {{links.text}}
+                        <div class="border-hover"></div>
                     </a>
                 </li>
             </ul>
@@ -77,9 +78,53 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
 header {
     background-color: #fff;
+    font-size: 0.7rem;
+    height: 95px;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        list-style-type: none;
+        height: 95px;
+
+        li {
+            position: relative;
+            height: 95px;
+            padding: 0 10px;
+
+            &:hover .border-hover {
+                background-color: $hover;
+                height: 4px;
+                width: 35px;
+                position: absolute;
+                bottom: 0;
+            }
+            
+            &:hover a {
+                color: $hover;
+            }
+
+        }
+
+        li a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            text-transform: uppercase;
+            color: #5E5F5E;
+            font-weight: bold;
+        }
+    }
 }
 
 </style>
