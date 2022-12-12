9cc97cd88d088bf31f9833aa22b4b2770496f4dd
Vale <oirelav95@gmail.com>
2022-12-12 15:17:04 +0100

    aggiungo logo nell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,15 +2,23 @@
 export default {
     data() {
         return {
+            imgLogo: 'dc-logo.png',
         };
     },
+    methods: {
+        getImagePath: function(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
 };
 </script>
 
 <template>
 
     <header>
-        
+        <a href="#">
+            <img :src="getImagePath(`/img/${imgLogo}`)" alt="Logo DC">
+        </a>
     </header>
     
 </template>
