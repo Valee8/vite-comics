88db9e7cdd5dda7cc938704ad19ee9c6f62d1d7f
Vale <oirelav95@gmail.com>
2022-12-12 16:59:55 +0100

    aggiungo sfondo al footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,3 +15,15 @@ export default {
 
     </footer>
 </template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+footer {
+    background-image: url('/img/dc-logo-bg.png'), url('/img/footer-bg.jpg');
+    background-repeat: no-repeat, repeat-x;
+    background-position: 75%, center;
+    background-size: 30%, cover;
+    min-height: 300px;
+}
+</style>
