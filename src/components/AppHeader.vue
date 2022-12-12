a2209f7648d45b9199a06d8378781ef0e907995d
Vale <oirelav95@gmail.com>
2022-12-12 15:24:51 +0100

    aggiungo nav con i links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,49 @@ export default {
     data() {
         return {
             imgLogo: 'dc-logo.png',
+            headerLinks: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                }
+
+            ]
         };
     },
     methods: {
@@ -19,6 +62,16 @@ export default {
         <a href="#">
             <img :src="getImagePath(`/img/${imgLogo}`)" alt="Logo DC">
         </a>
+
+        <nav>
+            <ul>
+                <li v-for="links in headerLinks">
+                    <a :href="links.url">
+                        {{links.text}}
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </header>
     
 </template>
