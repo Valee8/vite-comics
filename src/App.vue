606f4d830c828caafc4f32bb79d2579cd8305bc3
Vale <oirelav95@gmail.com>
2022-12-13 13:19:58 +0100

    creo nuovo componente ListSeries

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import ListSeries from './components/ListSeries.vue'
 import AppIcons from './components/AppIcons.vue'
 import AppFooter from './components/AppFooter.vue'
 
@@ -7,8 +8,9 @@ export default {
   name: "App",
   components: {
     AppHeader,
+    ListSeries,
     AppIcons,
-    AppFooter
+    AppFooter,
   }
 }
 </script>
@@ -17,14 +19,19 @@ export default {
 
   <AppHeader />
 
+  <main>
+
+    <ListSeries />
+
+  </main>
+
   <AppIcons />
 
   <AppFooter />
-  
+
 </template>
 
 <style lang="scss">
 @use './styles/partials/variables' as *;
 @use './styles/general.scss' as *;
-
 </style>
